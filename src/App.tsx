a9eebc51ab082791e9c8121b2788b53a98c88510
kaiyuan0708 <kaiyuan6953@gmail.com>
2025-10-12 03:26:22 +0800

    CSS - header

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ function App() {
   return (
     <Router>
       <Header />
-      <AppRoutes />
+      <main
+        style={{ paddingTop: "70px", maxWidth: "1000px", margin: "0 auto" }}
+      >
+        <AppRoutes />
+      </main>
     </Router>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
+import styles from "./Header.module.css";
 
 export function Header() {
-    return (
-        <header>
-            <nav>
-                <Link to="/">University</Link>
-                <Link to="/favorites">Favorites</Link>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <h1 className={styles.logo}>🎓 UniFinder</h1>
+        <div className={styles.links}>
+          <Link to="/" className={styles.link}>
+            Universities
+          </Link>
+          <Link to="/favorites" className={styles.link}>
+            Favorites
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
