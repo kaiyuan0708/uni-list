1c190364398869b75561fdf36a86d46d74194558
kaiyuan0708 <kaiyuan6953@gmail.com>
2025-10-12 14:04:03 +0800

    Fix - modify main css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router } from "react-router";
 import { Header } from "./components/Header";
 import { AppRoutes } from "./routes/AppRoutes";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Header />
-      <main
-        style={{ paddingTop: "70px", maxWidth: "1000px", margin: "0 auto" }}
-      >
+      <main className="app-container">
         <AppRoutes />
       </main>
     </Router>
