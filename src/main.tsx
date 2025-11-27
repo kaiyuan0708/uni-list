67d530f3381a73477acd53464f5e31658fb8bf35
kaiyuan0708 <kaiyuan6953@gmail.com>
2025-11-27 15:04:09 +0800

    [#p1] move Router to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router";
 import App from "./App.tsx";
 import { FavoritesProvider } from "./context/FavoritesContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <FavoritesProvider>
-      <App />
-    </FavoritesProvider>
+    <Router>
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
+    </Router>
   </StrictMode>
 );
